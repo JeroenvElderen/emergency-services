6c895ebf39a074a92f2509042deb5a6dc260a5b9
JeroenvElderen <jeroen.vanelderen@hotmail.com>
2026-04-27 18:37:27 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -407,8 +407,9 @@ const initialState: GameState = {
   },
 };
 
-const rand = (min: number, max: number) =>
-  Math.floor(Math.random() * (max - min + 1)) + min;
+function rand(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 function getRushHourModifier() {
   const now = new Date();
